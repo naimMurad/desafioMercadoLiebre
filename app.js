c61a2ca7d41fa1ad06d61bd6ab47f05fb6a6945f
Naim Murad <cachomurad@gmail.com>
2023-11-07 20:11:47 -0300

    Página Mercado Liebre casi terminada

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html")
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html")
+});
+
+app.listen(PORT, () => {
+    console.log("Escuchando al puerto " + PORT);
+});
